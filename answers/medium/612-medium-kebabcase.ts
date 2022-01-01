3d21e1f81bbba97a444db2c1c3f2ac6dbd0d2f93
pione30 <g.pionelish30@gmail.com>
2022-01-01 23:52:24 +0900

    612-medium-kebabcase

---
+++ answers/medium/612-medium-kebabcase.ts
@@ -0,0 +1,75 @@
+/*
+  612 - KebabCase
+  -------
+  by Johnson Chu (@johnsoncodehk) #medium #template-literal
+  
+  ### Question
+  
+  `FooBarBaz` -> `foo-bar-baz`
+  
+  > View on GitHub: https://tsch.js.org/612
+*/
+
+/* _____________ Your Code Here _____________ */
+type CapitalChar =
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | "G"
+  | "H"
+  | "I"
+  | "J"
+  | "K"
+  | "L"
+  | "M"
+  | "N"
+  | "O"
+  | "P"
+  | "Q"
+  | "R"
+  | "S"
+  | "T"
+  | "U"
+  | "V"
+  | "W"
+  | "X"
+  | "Y"
+  | "Z";
+
+type KebabCaseWithOptionalPrefix<S extends string> =
+  S extends `${infer Head}${infer Tail}`
+    ? Head extends CapitalChar
+      ? `-${Lowercase<Head>}${KebabCaseWithOptionalPrefix<Tail>}`
+      : `${Head}${KebabCaseWithOptionalPrefix<Tail>}`
+    : S;
+
+type KebabCase<S extends string> = S extends "-"
+  ? S
+  : KebabCaseWithOptionalPrefix<S> extends `-${infer R}`
+  ? R
+  : KebabCaseWithOptionalPrefix<S>;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<KebabCase<"FooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"fooBarBaz">, "foo-bar-baz">>,
+  Expect<Equal<KebabCase<"foo-bar">, "foo-bar">>,
+  Expect<Equal<KebabCase<"foo_bar">, "foo_bar">>,
+  Expect<Equal<KebabCase<"Foo-Bar">, "foo--bar">>,
+  Expect<Equal<KebabCase<"ABC">, "a-b-c">>,
+  Expect<Equal<KebabCase<"-">, "-">>,
+  Expect<Equal<KebabCase<"">, "">>,
+  Expect<Equal<KebabCase<"😎">, "😎">>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/612/answer
+  > View solutions: https://tsch.js.org/612/solutions
+  > More Challenges: https://tsch.js.org
+*/
